f37c221709d476aaeb0e8834bd166c9ad052c575
bactran-chch <158534203+bactran-chch@users.noreply.github.com>
2025-04-07 13:47:40 +1200

    add route 2

--- index.js
+++ index.js
@@ -13,26 +13,37 @@ const PRIVATE_APP_ACCESS = '';
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-    app.get('/', async (req, res) => {
-        const url = 'https://api.hubapi.com/crm/v3/objects/plant?properties=name,plant_type,plant_description';
-        const headers = {
-          Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-          'Content-Type': 'application/json'
-        };
-      
-        try {
-          const response = await axios.get(url, { headers });
-          const data = response.data.results;
-          res.render('homepage', { title: 'Plant Table | HubSpot Custom Object', data });
-        } catch (error) {
-          console.error(error.response?.data || error.message);
-          res.status(500).send('Error loading Plant data.');
-        }
-      });
+
+app.get('/', async (req, res) => {
+    const url = 'https://api.hubapi.com/crm/v3/objects/plant?properties=name,plant_type,plant_description';
+    const headers = {
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+    };
+  
+    try {
+      const response = await axios.get(url, { headers });
+      const data = response.data.results;
+      res.render('homepage', { title: 'Plant Table | HubSpot Custom Object', data });
+    } catch (error) {
+      console.error(error.response?.data || error.message);
+      res.status(500).send('Error loading Plant data.');
+    }
+  });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+
+app.get('/update-cobj', async (req, res) => {
+    try {
+      res.render('updates', {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
